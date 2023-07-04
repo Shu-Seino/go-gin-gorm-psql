7351defdd8ebd7633ea4b6046a8755929e95a8e4
Shu-Seino <shuuublog4649@gmail.com>
2023-07-04 12:33:41 +0000

    gormを追加

--- main.go
+++ main.go
@@ -1,33 +1,48 @@
 package main
 
 import (
-	"database/sql"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/joho/godotenv"
 	"os"
-	// "fmt"
-	"github.com/gin-gonic/gin"
-	_ "github.com/lib/pq"
 	"log"
 	"strconv"
 )
 
 type Member struct {
-	ID   int
+	gorm.Model
 	Name string
 }
 
-func insertMember(db *sql.DB, id int, name string) error {
-	_, err := db.Exec("INSERT INTO member (id, name) VALUES ($1, $2)", id, name)
+var db *gorm.DB
+
+func init() {
+	godotenv.Load(".env")
+	POSTGRES_USER,POSTGRES_PASSWORD,POSTGRES_DB := os.Getenv("POSTGRES_USER"),os.Getenv("POSTGRES_PASSWORD"),os.Getenv("POSTGRES_DB")
+	conn, err := gorm.Open("postgres", "user="+POSTGRES_USER+" dbname="+POSTGRES_DB+" password="+POSTGRES_PASSWORD+" sslmode=disable")
 	if err != nil {
-		return err
+		log.Fatal("Failed to connect to database:", err)
 	}
-	return nil
+	db = conn
+	db.AutoMigrate(&Member{})
 }
 
 func insertHandler(c *gin.Context) {
-	idStr := c.PostForm("id")
 	name := c.PostForm("name")
 
+	member := Member{Name: name}
+	if err := db.Create(&member).Error; err != nil {
+		log.Println(err)
+		c.String(500, "Internal Server Error")
+		return
+	}
+
+	c.Redirect(302, "/members")
+}
+
+func deleteHandler(c *gin.Context) {
+	idStr := c.Param("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
 		log.Println(err)
@@ -35,81 +50,38 @@ func insertHandler(c *gin.Context) {
 		return
 	}
 
-	// データベース接続のセットアップ
-	godotenv.Load(".env")
-	POSTGRES_USER,POSTGRES_PASSWORD,POSTGRES_DB := os.Getenv("POSTGRES_USER"),os.Getenv("POSTGRES_PASSWORD"),os.Getenv("POSTGRES_DB")
-	db, err := sql.Open("postgres", "user="+POSTGRES_USER+" dbname="+POSTGRES_DB+" password="+POSTGRES_PASSWORD+" sslmode=disable")
-	if err != nil {
-		log.Println(err)
-		c.String(500, "Internal Server Error")
-		return
-	}
-	defer db.Close()
-
-	// メンバーの追加
-	err = insertMember(db, id, name)
-	if err != nil {
+	if err := db.Delete(&Member{}, id).Error; err != nil {
 		log.Println(err)
 		c.String(500, "Internal Server Error")
 		return
 	}
 
-	// メンバーリストの表示ページにリダイレクト
 	c.Redirect(302, "/members")
 }
 
 func membersHandler(c *gin.Context) {
-	// データベース接続のセットアップ
-    godotenv.Load(".env")
-	POSTGRES_USER,POSTGRES_PASSWORD,POSTGRES_DB := os.Getenv("POSTGRES_USER"),os.Getenv("POSTGRES_PASSWORD"),os.Getenv("POSTGRES_DB")
-	db, err := sql.Open("postgres", "user="+POSTGRES_USER+" dbname="+POSTGRES_DB+" password="+POSTGRES_PASSWORD+" sslmode=disable")
-	if err != nil {
+	var members []Member
+	if err := db.Find(&members).Error; err != nil {
 		log.Println(err)
 		c.String(500, "Internal Server Error")
 		return
 	}
-	defer db.Close()
 
-	// メンバーリストの取得
-	rows, err := db.Query("SELECT * FROM member")
-	if err != nil {
-		log.Println(err)
-		c.String(500, "Internal Server Error")
-		return
-	}
-	defer rows.Close()
-
-	memberList := []Member{}
-
-	for rows.Next() {
-		var member Member
-		err := rows.Scan(&member.ID, &member.Name)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		memberList = append(memberList, member)
-	}
-
-	// HTMLテンプレートにデータを渡してレンダリング
 	c.HTML(200, "members.html", gin.H{
-		"MemberList": memberList,
+		"MemberList": members,
 	})
 }
 
 func main() {
 	r := gin.Default()
 
-	// 静的ファイルの配信
-	r.Static("/static", "./static")
+	r.Static("/static","./static")
 
-	// HTMLテンプレートのロード
 	r.LoadHTMLGlob("templates/*")
 
-	// メンバーリストのルーティングとハンドラの設定
 	r.GET("/members", membersHandler)
 	r.POST("/members", insertHandler)
+	r.DELETE("/members/:id", deleteHandler)
 
-	// サーバーの起動
 	r.Run(":8080")
 }
